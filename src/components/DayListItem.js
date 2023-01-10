590730049b376ab531c7bd7a29dcc353f18b370b
JeremyWallace1 <jeremy.wallace@telus.com>
2023-01-10 09:30:51 -0700

    adding attributes and onClick function to DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,9 +3,9 @@ import React from "react";
 // takes in three attributes (name, spots, selected) and one action (setDay)
 export default function DayListItem(props) {
   return (
-    <li>
-      <h2 className="text--regular">Day Name</h2> 
-      <h3 className="text--light">X spots remaining</h3>
+    <li onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{props.spots} spots remaining</h3>
     </li>
   );
 }
