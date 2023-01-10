284ecebd5734b7b23dd22f1adba8336435338953
JeremyWallace1 <jeremy.wallace@telus.com>
2023-01-10 09:51:09 -0700

    adding formatting to number of spots remaining to handle cases of no spots remaining and 1 spot remaining

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -5,10 +5,26 @@ import classNames from "classnames";
 // takes in three attributes (name, spots, selected) and one action (setDay)
 export default function DayListItem(props) {
   const dayClass = classNames('day-list__item', { "day-list__item--selected": props.selected, "day-list__item--full": props.spots === 0 });
+
+  const formatSpots = (props) => {
+    let spotsRemaining = "";
+    switch (props.spots){
+      case 0:
+        spotsRemaining = "no spots remaining";
+        break;
+      case 1:
+        spotsRemaining = "1 spot remaining";
+        break;
+      default:
+        spotsRemaining = `${props.spots} spots remaining`;
+    }
+    return spotsRemaining;
+  };
+
   return (
     <li onClick={() => props.setDay(props.name)} className={dayClass}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props)}</h3>
     </li>
   );
 }
