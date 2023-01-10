ee5733472f27d0a379ddf0e40f1bca8209f5f506
JeremyWallace1 <jeremy.wallace@telus.com>
2023-01-10 10:05:54 -0700

    adding DayList.js

---
+++ src/components/DayList.js
@@ -0,0 +1,16 @@
+/* Our <DayList> component will take in three props.
+
+- days: Array an array of objects (each object represents a day and includes an id, name, and spots)
+- day: String the currently selected day
+- setDay: Function sets the currently selected day and accepts the name of the day eg. "Monday", "Tuesday" */
+
+import React from "react";
+import "components/DayListItem.js";
+
+export default function DayList() {
+  return (
+    <ul>
+
+    </ul>
+  );
+};
