2d48494022e20cf0b2d58292fe3e9b5578f16e6c
JeremyWallace1 <jeremy.wallace@telus.com>
2023-01-10 10:49:28 -0700

    iterating over days array to create DayListItem <li>'s and testing in storybook

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -5,12 +5,23 @@
 - setDay: Function sets the currently selected day and accepts the name of the day eg. "Monday", "Tuesday" */
 
 import React from "react";
-import "components/DayListItem.js";
+import DayListItem from "components/DayListItem.js";
 
-export default function DayList() {
+export default function DayList(props) {
+  // iterate over the props.days array to get individual DayListItem <li>'s displayed
+  const mapDays = props.days.map((day) =>
+    <DayListItem 
+      key={day.id}
+      name={day.name}
+      spots={day.spots}
+      selected={day.name === props.day}
+      setDay={props.setDay}
+    />
+  );
+
   return (
     <ul>
-
+      {mapDays}
     </ul>
   );
 };
