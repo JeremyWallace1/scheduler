d559df714bc5c9b4783ee8a4a39343be9f5e695e
JeremyWallace1 <jeremy.wallace@telus.com>
2023-01-11 10:17:19 -0700

    creating the confirm dialog and adding to stories

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
 
 storiesOf("Button", module)
   .addParameters({
@@ -148,3 +149,4 @@ storiesOf("Button", module)
       .add("Empty", () => <Empty
         onAdd={action("onAdd")} />)
       .add("Show", () => <Show student="Lydia Miller-Jones" interviewer={interviewer.name} onEdit={action("onEdit")} onDelete={action("onDelete")} />)
+      .add("Confirm", () => <Confirm message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")} />)
