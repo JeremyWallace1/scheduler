1bc88e12db4ea654c37e17c6919c18b68b034f35
JeremyWallace1 <jeremy.wallace@telus.com>
2023-01-12 14:54:11 -0700

    removing console.log from selectors.js

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -4,7 +4,6 @@ export function getAppointmentsForDay(state, day) {
   for (let i = 0; i < state.days.length; i++) {
     if (state.days[i].name === day) {
       appointments.push(...state.days[i].appointments);
-      console.log("appointments:", appointments);
     }
   }
   const appointmentsForDay = appointments.map(x => state.appointments[x]);
