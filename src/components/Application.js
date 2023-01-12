5963adbe043aa9dc4dd505db5ca44d214cecd1b9
JeremyWallace1 <jeremy.wallace@telus.com>
2023-01-12 15:09:11 -0700

    refactored code to put all states together, updating variables and creating functions for setDay and setDays and removing dependency by using prev in setDays for axios request in useEffect

--- src/components/Application.js
+++ src/components/Application.js
@@ -44,8 +44,11 @@ const appointments = {
 };
 
 const Application = (props) => {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
 
   useEffect(() => {
     axios.get("/api/days").then((response) => {
@@ -60,6 +63,9 @@ const Application = (props) => {
     />
   );
 
+  const setDay = day => setState({ ...state, day });
+
+  const setDays = days => setState(prev => ({ ...prev, days }));
 
   return (
     <main className="layout">
@@ -72,8 +78,8 @@ const Application = (props) => {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
           />
         </nav>
