5ddd1a3ef66da5357aefbae2186a346a95645012
JeremyWallace1 <jeremy.wallace@telus.com>
2023-01-13 14:41:34 -0700

    refactoring useVisualMode so no longer mutating mode and history

--- src/hooks/useVisualMode.jsx
+++ src/hooks/useVisualMode.jsx
@@ -3,39 +3,28 @@ import { useState } from "react";
 const useVisualMode = (initial) => {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
-
+ 
   const transition = (newMode, replace = false) => {
     if (!replace) {
-      history.push(mode);
+        setHistory([...history, mode])
     }
     setMode(newMode);
-    console.log("line 19: history:", history, "mode:", mode, "replace:", replace);
   };
 
   const back = () => {
+    const newHistory = history;
     if (history.length >= 1) {
-      setMode(history[history.length - 1]);
-      history.pop();
-    }
-    console.log('line 27: history:', history, 'mode:', mode);
-  };
+      const newMode = newHistory[newHistory.length - 1];
+      setMode(newMode);
+    } 
+    newHistory.pop();
+    setHistory([...newHistory])
+  }
 
   return {
     mode, transition, back
   };
 };
 
-// const useVisualMode = (initial) => {
-//   const [mode, setMode] = useState(initial);
-//   console.log("initial:", initial, "transition:", transition);
-//   setMode(initial);
-//   const transition = (value) => {
-//     setMode(value);
-//   };
-//   console.log('mode:', mode);
-//   return {
-//     mode
-//   };
-// };
 
 export default useVisualMode;
